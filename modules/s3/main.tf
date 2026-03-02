319d980aa9848bab571ef4e6d5484e281c7a1f94
池ノ谷優太 <you1207.ike@gmail.com>
2026-03-02 13:36:26 +0900

    【テスト】S3作成

---
+++ modules/s3/main.tf
@@ -0,0 +1,18 @@
+# S3のbucketを作成する
+resource "aws_s3_bucket" "front" {
+  bucket = "terraform-sample-bucket"
+
+  tags = {
+    Name = "terraform-sample"
+  }
+}
+
+# パブリックアクセスをブロックする設定
+resource "aws_s3_bucket_public_access_block" "front" {
+  bucket                  = aws_s3_bucket.front.bucket
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
